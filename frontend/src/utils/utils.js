077112d172f937eefadd7576e31dcd317bac205f
zack <cai.guanyu@qq.com>
2019-10-23 23:20:57 +0800

    :construction: 添加utils.js

---
+++ frontend/src/utils/utils.js
@@ -0,0 +1,9 @@
+export const getRelativePathAndParams = () => {
+    var url = document.location.toString();
+    var arrUrl = url.split("//");
+
+    var start = arrUrl[1].indexOf("/");
+    var relUrl = arrUrl[1].substring(start);
+    
+    return relUrl;
+}
