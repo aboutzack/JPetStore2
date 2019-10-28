e7f0ba0b023bbac474060541960b4cce20b1ad1b
zack <cai.guanyu@qq.com>
2019-10-28 23:41:18 +0800

    :construction: 修复api路径

--- frontend/src/components/index-content.vue
+++ frontend/src/components/index-content.vue
@@ -3,8 +3,11 @@
     <div class="input-search"><input-search></input-search></div>
     <div class="block">
       <el-carousel height="350px">
-        <el-carousel-item v-for="item in 4" :key="item">
+        <el-carousel-item v-for="item in 3" :key="item">
           <h3 class="small"><img src="https://www.thesprucepets.com/thmb/5qqmdB9NoLp5CVOpUOQiwXRagAQ=/1080x1222/filters:no_upscale()/31170124_355309831644176_4994517524629422080_n-5b3a4d0ec9e77c003771e6a6.jpg"></h3>
+        </el-carousel-item>
+        <el-carousel-item>
+          <h3 class="small"><img src="http://www.lady98.com/upload/2015/06/05/145b326f-57b9-498e-9ff8-988f58cf161d.jpg"></h3>
         </el-carousel-item>
       </el-carousel>
     </div>
@@ -47,12 +50,5 @@ export default {
 img {
   width: 100%;
   height: 70%;
-  /* width: 100%;
-	height: auto;
-	top:50%;
-	-webkit-transform: translateY(-50%);
-	-ms-transform: translateY(-50%);
-	-moz-transform: translateY(-50%);
-	position: relative; */
 }
 </style>
