867163ad1ed7cdd01b05045b711a472a9c7904c8
zack <cai.guanyu@qq.com>
2020-03-27 17:41:10 +0800

    typo

--- backend/src/main/java/org/csu/jpetstore/controller/AccountController.java
+++ backend/src/main/java/org/csu/jpetstore/controller/AccountController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-public class AccoutController {
+public class AccountController {
     @Autowired
     AccountService accountService;
     @Autowired
